98ffc98c7851e0e930d396eae976909557a00a90
kohsuke <kohsuke@@dev.java.net>
2004-10-31 03:30:35 +0000

    expanded javadoc

--- src/org/kohsuke/stapler/tags/Include.java
+++ src/org/kohsuke/stapler/tags/Include.java
@@ -14,12 +14,44 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
+ * Includes a side JSP file for the "it" object.
+ *
+ * <p>
+ * This tag looks for a side JSP file of the given name
+ * from the inheritance hierarchy of the it object,
+ * and includes the contents of it, just like &lt;jsp:include>.
+ *
+ * <p>
+ * For example, if the "it" object is an instance of the <tt>Foo</tt> class,
+ * which looks like the following:
+ *
+ * <pre>
+ * class Foo extends Bar { ... }
+ * class Bar extends Zot { ... }
+ * </pre>
+ *
+ * <p>
+ * And if you write:
+ * <pre><xmp>
+ * <st:include page="abc.jsp"/>
+ * </xmp></pre>
+ * then, it looks for the following files in this order,
+ * and includes the first one found.
+ * <ol>
+ *  <li>a side-file of the Foo class named abc.jsp (/WEB-INF/side-files/Foo/abc.jsp)
+ *  <li>a side-file of the Bar class named abc.jsp (/WEB-INF/side-files/Bar/abc.jsp)
+ *  <li>a side-file of the Zot class named abc.jsp (/WEB-INF/side-files/Zot/abc.jsp)
+ * </ol>
+ *
  * @author Kohsuke Kawaguchi
  */
 public class Include extends SimpleTagSupport {
 
     private String page;
 
+    /**
+     * Specifies the name of the JSP to be included.
+     */
     public void setPage(String page) {
         this.page = page;
     }
