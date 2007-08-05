bf6710813302002076c1b7e604b5b7b64ecdf317
kohsuke <kohsuke@@dev.java.net>
2007-08-05 16:01:41 +0000

    updated shell scripts

--- release.sh
+++ release.sh
@@ -7,3 +7,4 @@
 mvn release:prepare release:perform
 ver=$(show-pom-version target/checkout/pom.xml)
 javanettasks uploadFile stapler /stapler-$ver.zip "$ver release" stable target/checkout/target/stapler-$ver-bin.zip
+./push-javadoc.sh
