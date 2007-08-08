86e5428b6fd2d4b952601431f73b9c8297c95354
kohsuke <kohsuke@@dev.java.net>
2007-08-08 19:48:46 +0000

    cleaned up

--- release.sh
+++ release.sh
@@ -1,9 +1,4 @@
 #!/bin/sh -ex
-#maven clean dist source
-#maven javanet:deploy-jar javanet:deploy-java-source
-# mvn clean deploy
-# javanet:dist
-
 mvn -B release:prepare release:perform
 ver=$(show-pom-version target/checkout/pom.xml)
 javanettasks uploadFile stapler /stapler-$ver.zip "$ver release" stable target/checkout/target/stapler-$ver-bin.zip
