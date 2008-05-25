753330f8b1a6b1ec44183236d933ec93496a11f9
kohsuke <kohsuke@@dev.java.net>
2008-05-25 21:17:37 +0000

    getOutputStream is used to create the XMLOutput, so can't use getWriter

--- src/org/kohsuke/stapler/jelly/DoctypeTag.java
+++ src/org/kohsuke/stapler/jelly/DoctypeTag.java
@@ -14,14 +14,17 @@ public class DoctypeTag extends AbstractStaplerTag {
     private String publicId;
     private String systemId;
 
-    public DoctypeTag(String publicId, String systemId) {
+    public void setPublicId(String publicId) {
         this.publicId = publicId;
+    }
+
+    public void setSystemId(String systemId) {
         this.systemId = systemId;
     }
 
     public void doTag(XMLOutput output) throws JellyTagException {
         try {
-            getResponse().getWriter().println("<!DOCTYPE html PUBLIC \""+publicId+"\" \""+systemId+"\">");
+            getResponse().getOutputStream().println("<!DOCTYPE html PUBLIC \""+publicId+"\" \""+systemId+"\">");
         } catch (IOException e) {
             throw new JellyTagException(e);
         }
