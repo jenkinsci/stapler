d53d87409ad29f6f63ad6707833edfe6833fdcce
kohsuke <kohsuke@@dev.java.net>
2008-06-01 17:51:46 +0000

    copied from Hudson

---
+++ src/org/kohsuke/stapler/framework/io/IOException2.java
@@ -0,0 +1,21 @@
+package org.kohsuke.stapler.framework.io;
+
+import java.io.IOException;
+
+/**
+ * {@link IOException} with missing constructors.
+ * @author Kohsuke Kawaguchi
+ */
+public class IOException2 extends IOException {
+    public IOException2() {
+    }
+
+    public IOException2(String message) {
+        super(message);
+    }
+
+    public IOException2(String message, Throwable cause) {
+        super(message);
+        initCause(cause);
+    }
+}
