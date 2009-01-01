3540413101a49e7d1d52e6ffb30f3a8c38d58534
kohsuke <kohsuke@@dev.java.net>
2009-01-01 14:34:57 +0000

    release script should push javadoc

--- release.sh
+++ release.sh
@@ -2,4 +2,8 @@
 mvn -B release:prepare release:perform
 ver=$(show-pom-version target/checkout/pom.xml)
 javanettasks uploadFile stapler /stapler-$ver.zip "$ver release" stable target/checkout/target/stapler-$ver-bin.zip
-./push-javadoc.sh
+cp -R target/checkout/target/apidocs/* ../www/javadoc
+cd ../www/javadoc
+rcvsadd . "pushing javadoc for $ver"
+
+
