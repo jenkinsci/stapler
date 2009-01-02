4d7a85863d90da30bd46e735dd8ce7cf9d2011a8
kohsuke <kohsuke@@dev.java.net>
2009-01-02 19:39:25 +0000

    switched to Maven-based site generation with some static HTMLs left in www

--- release.sh
+++ release.sh
@@ -2,8 +2,8 @@
 mvn -B release:prepare release:perform
 ver=$(show-pom-version target/checkout/pom.xml)
 javanettasks uploadFile stapler /stapler-$ver.zip "$ver release" stable target/checkout/target/stapler-$ver-bin.zip
-cp -R target/checkout/target/apidocs/* ../www/javadoc
-cd ../www/javadoc
+cp -R target/checkout/target/site/* ../www
+cd ../www
 rcvsadd . "pushing javadoc for $ver"
 
 
