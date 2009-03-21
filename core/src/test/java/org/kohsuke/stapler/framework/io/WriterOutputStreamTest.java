25f109517ef29440e65b0fd576223dcdd07a789e
kohsuke <kohsuke@@dev.java.net>
2009-03-21 01:53:22 +0000

    making Surefire happy

--- core/src/test/java/org/kohsuke/stapler/framework/io/WriterOutputStreamTest.java
+++ core/src/test/java/org/kohsuke/stapler/framework/io/WriterOutputStreamTest.java
@@ -1,16 +1,20 @@
 package org.kohsuke.stapler.framework.io;
 
+import junit.framework.TestCase;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 
 /**
- * TODO: make it a junit test
+ * TODO: make it a real junit test
  *
  * @author jglick
  */
-public class WriterOutputStreamTest {
+public class WriterOutputStreamTest extends TestCase {
+    public void testFoo() {} // otherwise surefire will be unhappy
+    
     public static void main(String[] args) throws IOException {
         OutputStream os = new WriterOutputStream(new OutputStreamWriter(System.out));
         PrintStream ps = new PrintStream(os);
