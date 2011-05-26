4f7e5fdb6c2ef1660738fa02f1a7836aef0c6f8c
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-05-27 02:19:43 +0900

    Split out ERB specific part from templating logic
    Preparing for Haml engine support.

--- jruby/src/main/resources/org/kohsuke/stapler/jelly/jruby/JRubyJellyScriptImpl.rb
+++ jruby/src/main/resources/org/kohsuke/stapler/jelly/jruby/JRubyJellyScriptImpl.rb
@@ -1,37 +1,33 @@
 require 'erb'
 require 'java'
 
-class JRubyJellyScriptImpl < org::kohsuke::stapler::jelly::jruby::JRubyJellyScript
-  def initialize(template)
-    super()
-    @template = ERB.new(template,nil,nil,"self.output")
-  end
+module JRubyJellyScriptImpl
 
-  def run(jelly_context,xml_output)
-    begin
-      JRubyContext.new(self,jelly_context,xml_output).evaluate_template(@template)
-    rescue Exception => ex
-      raise org.apache.commons.jelly.JellyTagException.new(ex.message)
+  class OutputStream
+    attr_accessor :output
+
+    def initialize(output)
+      @output = output
     end
   end
 
   class JRubyContext
     include org::kohsuke::stapler::jelly::jruby::IJRubyContext
 
-    attr_accessor :context,:xml,:script
+    attr_accessor :script, :context, :stream
 
-    def initialize(script,context,xml)
+    def initialize(script, context, stream)
       @script = script
       @context = context
-      @xml = xml
+      @stream = stream
     end
 
-    def evaluate_template(erb)
-      erb.result(WriterBinding.new(self).instance_eval{binding})
+    def evaluate(&block)
+      yield(WriterBinding.new(self).instance_eval{binding})
     end
 
     def getJellyContext()
-      @context;
+      @context
     end
 
     def setJellyContext(context)
@@ -39,63 +35,96 @@ class JRubyJellyScriptImpl < org::kohsuke::stapler::jelly::jruby::JRubyJellyScri
     end
 
     def getOutput()
-      @output
+      @stream.output
     end
 
     def setOutput(output)
-      @output = output
+      @stream.output = output
     end
   end
 
   # variables exposed to ERB
   class WriterBinding
-    attr_reader :output
-
     def initialize(context)
-      @output = self
       @context = context
     end
 
-    def output=(_)
-      # ignoring the assignment, because we want our concat method to be invoked
-    end
-
-    def context()
+    def context
       @context.context
     end
 
+    def stream
+      @context.stream
+    end
+
     # this is where we handle all the variable references
-    def method_missing(name,*args)
+    def method_missing(name, *args)
       # variables defined in the current context
       v = context.getVariable(name.to_s)
       return v if v!=nil
-
       super # make it fail
     end
 
     # load taglib
     def taglib(uri)
       # TODO: cache
-      Taglib.new(@context,uri)
-    end
-
-    # ERB calls this method every time it wants to write some string
-    def concat(str)
-      @context.xml.write(str) if str!=nil
+      Taglib.new(@context, uri)
     end
   end
 
   # receives tag invocations as method calls
   class Taglib
-    def initialize(context,uri)
+    def initialize(context, uri)
       @context = context
       @uri = uri
     end
 
     # this is the actual taglib invocation like f.entry(:a => b, :c => d)
-    def method_missing(name,*args,&block)
-      @context.script.invokeTaglib(@context, @context.context, @context.xml, @uri, name.to_s, args[0], block)
+    def method_missing(name, *args, &block)
+      @context.script.invokeTaglib(@context, @context.context, @context.stream.output, @uri, name.to_s, args[0], block)
     end
   end
+
+  class JRubyJellyERbScript < org::kohsuke::stapler::jelly::jruby::JRubyJellyScript
+    def initialize(template)
+      super()
+      @engine = ERB.new(template, nil, nil, "stream.eoutvar")
+    end
+
+    def run(jelly_context,xml_output)
+      begin
+        ctx = JRubyContext.new(self, jelly_context, ERbOutputStream.new(xml_output))
+        ctx.evaluate { |b|
+          @engine.result(b)
+        }
+      rescue Exception => ex
+        raise org.apache.commons.jelly.JellyTagException.new(ex.message)
+      end
+    end
+
+    class ERbOutputStream < OutputStream
+      attr_accessor :output
+
+      def initialize(output)
+        super(output)
+      end
+
+      # ERB calls 'eoutvar.concat(str)'. Return self for intercepting 'concat' call.
+      def eoutvar
+        self
+      end
+
+      # ERB calls "eoutvar = ''" first. Ignore it and just use 'concat' calls.
+      def eoutvar=(eoutvar)
+        # ignore
+      end
+
+      # ERB calls this method every time it wants to write some string
+      def concat(str)
+        output.write(str) if str
+      end
+    end
+  end
+
 end
 
