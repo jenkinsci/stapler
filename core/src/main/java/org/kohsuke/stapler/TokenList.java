5f12207b02a9046381c7a53ae8c32ef5a342f312
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-04-04 19:17:54 -0700

    defense against directory traversal attack.

--- core/src/main/java/org/kohsuke/stapler/TokenList.java
+++ core/src/main/java/org/kohsuke/stapler/TokenList.java
@@ -40,7 +40,7 @@ public final class TokenList {
     public int idx;
 
     TokenList(String url) {
-        StringTokenizer tknzr = new StringTokenizer(url,"/");
+        StringTokenizer tknzr = new StringTokenizer(url,"/\\");
         final int tokenCount = tknzr.countTokens();
         tokens = new String[tokenCount];
         rawTokens = new String[tokenCount];
