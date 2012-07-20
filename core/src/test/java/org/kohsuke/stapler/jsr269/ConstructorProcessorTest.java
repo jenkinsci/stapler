fa09533ee7c68f8f194255711cba527d94e26f36
Jesse Glick <jglick@cloudbees.com>
2012-07-20 15:58:41 -0400

    More tests.

---
+++ core/src/test/java/org/kohsuke/stapler/jsr269/ConstructorProcessorTest.java
@@ -0,0 +1,38 @@
+package org.kohsuke.stapler.jsr269;
+
+import java.util.Collections;
+import net.java.dev.hickory.testing.Compilation;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ConstructorProcessorTest {
+
+    @Test public void basicOutput() {
+        Compilation compilation = new Compilation();
+        compilation.addSource("some.pkg.Stuff").
+                addLine("package some.pkg;").
+                addLine("import org.kohsuke.stapler.DataBoundConstructor;").
+                addLine("public class Stuff {").
+                addLine("  @DataBoundConstructor public Stuff(int count, String name) {}").
+                addLine("}");
+        compilation.doCompile(null, "-source", "6");
+        assertEquals(Collections.emptyList(), Utils.filterSupportedSourceVersionWarnings(compilation.getDiagnostics()));
+        assertEquals("{constructor=count,name}", Utils.normalizeProperties(Utils.getGeneratedResource(compilation, "some/pkg/Stuff.stapler")));
+    }
+
+    @Test public void preAnnotationCompatibility() {
+        Compilation compilation = new Compilation();
+        compilation.addSource("some.pkg.Stuff").
+                addLine("package some.pkg;").
+                addLine("public class Stuff {").
+                addLine("  /** @stapler-constructor */ public Stuff(String name, int count) {}").
+                addLine("}");
+        compilation.doCompile(null, "-source", "6");
+        assertEquals(Collections.emptyList(), Utils.filterSupportedSourceVersionWarnings(compilation.getDiagnostics()));
+        assertEquals("{constructor=name,count}", Utils.normalizeProperties(Utils.getGeneratedResource(compilation, "some/pkg/Stuff.stapler")));
+    }
+
+    // XXX nested classes use qualified rather than binary name
+    // XXX behavior when multiple @DataBoundConstructor's specified on a single class - error?
+
+}
