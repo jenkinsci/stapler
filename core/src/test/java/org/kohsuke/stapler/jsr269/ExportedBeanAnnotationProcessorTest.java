2859715f72f0b97db1cdeac68a78d0301109a8af
Jesse Glick <jglick@cloudbees.com>
2012-08-02 09:28:27 -0400

    JDK bug filed. http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=7188605

--- core/src/test/java/org/kohsuke/stapler/jsr269/ExportedBeanAnnotationProcessorTest.java
+++ core/src/test/java/org/kohsuke/stapler/jsr269/ExportedBeanAnnotationProcessorTest.java
@@ -56,7 +56,7 @@ public class ExportedBeanAnnotationProcessorTest {
                 addLine("}");
         compilation.doCompile(null, "-source", "6");
         assertEquals(Collections.emptyList(), Utils.filterSupportedSourceVersionWarnings(compilation.getDiagnostics()));
-        /* XXX currently broken in JDK 6 and org.jvnet.hudson:annotation-indexer:1.2:
+        /* #7188605: broken in JDK 6u33 + org.jvnet.hudson:annotation-indexer:1.2:
         assertEquals("some.pkg.Stuff\n", Utils.getGeneratedResource(compilation, "META-INF/annotations/org.kohsuke.stapler.export.ExportedBean"));
         */
         // XXX is it intentional that these are not listed here? (example: hudson.plugins.mercurial.MercurialSCM)
