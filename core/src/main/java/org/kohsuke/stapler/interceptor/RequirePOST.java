372f1500069292469306fc56b88149bd60e64976
Jesse Glick <jglick@cloudbees.com>
2013-07-23 13:56:20 -0400

    [JENKINS-16918] Friendlier error page that offers to submit the same URL as a POST request.

--- core/src/main/java/org/kohsuke/stapler/interceptor/RequirePOST.java
+++ core/src/main/java/org/kohsuke/stapler/interceptor/RequirePOST.java
@@ -1,5 +1,7 @@
 package org.kohsuke.stapler.interceptor;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
@@ -9,6 +11,7 @@ import java.lang.reflect.InvocationTargetException;
 
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import org.kohsuke.stapler.HttpResponses;
 
@@ -27,7 +30,17 @@ public @interface RequirePOST {
         public Object invoke(StaplerRequest request, StaplerResponse response, Object instance, Object[] arguments)
                 throws IllegalAccessException, InvocationTargetException {
             if (!request.getMethod().equals("POST")) {
-                throw new InvocationTargetException(HttpResponses.errorWithoutStack(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "POST is required for " + target.getQualifiedName()));
+                throw new InvocationTargetException(new HttpResponses.HttpResponseException() {
+                    @Override public void generateResponse(StaplerRequest req, StaplerResponse rsp, Object node) throws IOException, ServletException {
+                        rsp.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+                        rsp.setContentType("text/html");
+                        PrintWriter w = rsp.getWriter();
+                        w.println("<html><head><title>POST required</title></head><body>");
+                        w.println("POST is required for " + target.getQualifiedName() + "<br>");
+                        w.println("<form method='POST'><input type='submit' value='Try POSTing'></form>");
+                        w.println("</body></html>");
+                    }
+                });
             }
             return target.invoke(request, response, instance, arguments);
         }
