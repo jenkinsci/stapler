9f9c89320a2bd5c50818ba220b4211965e6bd0f7
Matt Moore <mattmoor@google.com>
2014-06-15 21:27:52 -0700

    rely on our owner for providing the WebApp (as we already do lower in the file), to avoid a dependency on WebApp.getCurrent() and transitively Stapler.getCurrent(), which mandate that this be executed from a request context.

--- jelly/src/main/java/org/kohsuke/stapler/jelly/JellyClassTearOff.java
+++ jelly/src/main/java/org/kohsuke/stapler/jelly/JellyClassTearOff.java
@@ -49,7 +49,7 @@ public class JellyClassTearOff extends AbstractTearOff<JellyClassLoaderTearOff,S
 
     public JellyClassTearOff(MetaClass owner) {
         super(owner,JellyClassLoaderTearOff.class);
-        facet = WebApp.getCurrent().getFacet(JellyFacet.class);
+        facet = owner.webApp.getFacet(JellyFacet.class);
     }
 
     protected Script parseScript(URL res) throws JellyException {
