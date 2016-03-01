d840f1d44c2a42aa090ae9d8661fd006e9765117
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-03-01 15:30:57 -0800

    Making this more object oriented

--- core/src/main/java/org/kohsuke/stapler/AcceptHeader.java
+++ core/src/main/java/org/kohsuke/stapler/AcceptHeader.java
@@ -24,11 +24,12 @@ package org.kohsuke.stapler;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * MIME-Type Parser
@@ -46,116 +47,115 @@ import java.util.List;
  * Ported by Tom Zellman <tzellman@gmail.com>.
  *
  */
-public final class MIMEParse
+public final class AcceptHeader
 {
+    private final List<Atom> atoms = new ArrayList<Atom>();
 
     /**
-     * Parse results container
+     * Parse the accept header value.
+     *
+     * @param ranges
+     *      something like "text/*;q=0.5,*; q=0.1"
      */
-    protected static class ParseResults
-    {
-        String type;
+    public AcceptHeader(String ranges) {
+        for (String r : StringUtils.split(ranges, ','))
+            atoms.add(Atom.parseMediaRange(r));
+    }
 
+    /**
+     * Media range plus parameters and extensions
+     */
+    protected static class Atom {
+        String type;
         String subType;
 
         // !a dictionary of all the parameters for the media range
         Map<String, String> params;
 
         @Override
-        public String toString()
-        {
+        public String toString() {
             StringBuffer s = new StringBuffer("('" + type + "', '" + subType
                     + "', {");
             for (String k : params.keySet())
                 s.append("'" + k + "':'" + params.get(k) + "',");
             return s.append("})").toString();
         }
-    }
 
-    /**
-     * Carves up a mime-type and returns a ParseResults object
-     *
-     * For example, the media range 'application/xhtml;q=0.5' would get parsed
-     * into:
-     *
-     * ('application', 'xhtml', {'q', '0.5'})
-     */
-    protected static ParseResults parseMimeType(String mimeType)
-    {
-        String[] parts = StringUtils.split(mimeType, ";");
-        ParseResults results = new ParseResults();
-        results.params = new HashMap<String, String>();
+        /**
+         * Carves up a mime-type and returns an Atom object
+         *
+         * For example, the media range 'application/xhtml;q=0.5' would get parsed
+         * into:
+         *
+         * ('application', 'xhtml', {'q', '0.5'})
+         */
+        protected static Atom parseMimeType(String mimeType) {
+            String[] parts = StringUtils.split(mimeType, ";");
+            Atom a = new Atom();
+            a.params = new HashMap<String, String>();
 
-        for (int i = 1; i < parts.length; ++i)
-        {
-            String p = parts[i];
-            String[] subParts = StringUtils.split(p, '=');
-            if (subParts.length == 2)
-                results.params.put(subParts[0].trim(), subParts[1].trim());
+            for (int i = 1; i < parts.length; ++i) {
+                String p = parts[i];
+                String[] subParts = StringUtils.split(p, '=');
+                if (subParts.length == 2)
+                    a.params.put(subParts[0].trim(), subParts[1].trim());
+            }
+            String fullType = parts[0].trim();
+
+            // Java URLConnection class sends an Accept header that includes a
+            // single "*" - Turn it into a legal wildcard.
+            if (fullType.equals("*"))
+                fullType = "*/*";
+            String[] types = StringUtils.split(fullType, "/");
+            a.type = types[0].trim();
+            a.subType = types[1].trim();
+            return a;
         }
-        String fullType = parts[0].trim();
 
-        // Java URLConnection class sends an Accept header that includes a
-        // single "*" - Turn it into a legal wildcard.
-        if (fullType.equals("*"))
-            fullType = "*/*";
-        String[] types = StringUtils.split(fullType, "/");
-        results.type = types[0].trim();
-        results.subType = types[1].trim();
-        return results;
+        /**
+         * Carves up a media range and returns a ParseResults.
+         *
+         * For example, the media range 'application/*;q=0.5' would get parsed into:
+         *
+         * ('application', '*', {'q', '0.5'})
+         *
+         * In addition this function also guarantees that there is a value for 'q'
+         * in the params dictionary, filling it in with a proper default if
+         * necessary.
+         *
+         * @param range
+         */
+        protected static Atom parseMediaRange(String range) {
+            Atom results = parseMimeType(range);
+            String q = results.params.get("q");
+            float f = NumberUtils.toFloat(q, 1);
+            if (StringUtils.isBlank(q) || f < 0 || f > 1)
+                results.params.put("q", "1");
+            return results;
+        }
     }
 
-    /**
-     * Carves up a media range and returns a ParseResults.
-     *
-     * For example, the media range 'application/*;q=0.5' would get parsed into:
-     *
-     * ('application', '*', {'q', '0.5'})
-     *
-     * In addition this function also guarantees that there is a value for 'q'
-     * in the params dictionary, filling it in with a proper default if
-     * necessary.
-     *
-     * @param range
-     */
-    protected static ParseResults parseMediaRange(String range)
-    {
-        ParseResults results = parseMimeType(range);
-        String q = results.params.get("q");
-        float f = NumberUtils.toFloat(q, 1);
-        if (StringUtils.isBlank(q) || f < 0 || f > 1)
-            results.params.put("q", "1");
-        return results;
-    }
 
     /**
      * Structure for holding a fitness/quality combo
      */
-    protected static class FitnessAndQuality implements
-            Comparable<FitnessAndQuality>
-    {
-        int fitness;
-
-        float quality;
-
+    protected static class FitnessAndQuality implements Comparable<FitnessAndQuality> {
+        private final int fitness;
+        private final float quality;
         String mimeType; // optionally used
 
-        public FitnessAndQuality(int fitness, float quality)
-        {
+        public FitnessAndQuality(int fitness, float quality) {
             this.fitness = fitness;
             this.quality = quality;
         }
 
-        public int compareTo(FitnessAndQuality o)
-        {
-            if (fitness == o.fitness)
-            {
+        public int compareTo(FitnessAndQuality o) {
+            if (fitness == o.fitness) {
                 if (quality == o.quality)
                     return 0;
                 else
                     return quality < o.quality ? -1 : 1;
-            }
-            else
+            } else
                 return fitness < o.fitness ? -1 : 1;
         }
     }
@@ -168,16 +168,13 @@ public final class MIMEParse
      * quality_parsed(), 'parsed_ranges' must be a list of parsed media ranges.
      *
      * @param mimeType
-     * @param parsedRanges
      */
-    protected static FitnessAndQuality fitnessAndQualityParsed(String mimeType,
-            Collection<ParseResults> parsedRanges)
-    {
+    protected FitnessAndQuality fitnessAndQualityParsed(String mimeType) {
         int bestFitness = -1;
         float bestFitQ = 0;
-        ParseResults target = parseMediaRange(mimeType);
+        Atom target = Atom.parseMediaRange(mimeType);
 
-        for (ParseResults range : parsedRanges)
+        for (Atom range : atoms)
         {
             if ((target.type.equals(range.type) || range.type.equals("*") || target.type
                     .equals("*"))
@@ -209,35 +206,13 @@ public final class MIMEParse
     }
 
     /**
-     * Find the best match for a given mime-type against a list of ranges that
-     * have already been parsed by parseMediaRange(). Returns the 'q' quality
-     * parameter of the best match, 0 if no match was found. This function
-     * bahaves the same as quality() except that 'parsed_ranges' must be a list
-     * of parsed media ranges.
-     *
-     * @param mimeType
-     * @param parsedRanges
-     * @return
-     */
-    protected static float qualityParsed(String mimeType,
-            Collection<ParseResults> parsedRanges)
-    {
-        return fitnessAndQualityParsed(mimeType, parsedRanges).quality;
-    }
-
-    /**
      * Returns the quality 'q' of a mime-type when compared against the
      * mediaRanges in ranges. For example:
      *
      * @param mimeType
-     * @param parsedRanges
      */
-    public static float quality(String mimeType, String ranges)
-    {
-        List<ParseResults> results = new LinkedList<ParseResults>();
-        for (String r : StringUtils.split(ranges, ','))
-            results.add(parseMediaRange(r));
-        return qualityParsed(mimeType, results);
+    public float quality(String mimeType) {
+        return fitnessAndQualityParsed(mimeType).quality;
     }
 
     /**
@@ -250,20 +225,13 @@ public final class MIMEParse
      * "text/xml"}), "text/*;q=0.5,*; q=0.1") 'text/xml'
      *
      * @param supported
-     * @param header
      * @return
      */
-    public static String bestMatch(Collection<String> supported, String header)
-    {
-        List<ParseResults> parseResults = new LinkedList<ParseResults>();
-        List<FitnessAndQuality> weightedMatches = new LinkedList<FitnessAndQuality>();
-        for (String r : StringUtils.split(header, ','))
-            parseResults.add(parseMediaRange(r));
+    public String bestMatch(Collection<String> supported) {
+        List<FitnessAndQuality> weightedMatches = new ArrayList<FitnessAndQuality>();
 
-        for (String s : supported)
-        {
-            FitnessAndQuality fitnessAndQuality = fitnessAndQualityParsed(s,
-                    parseResults);
+        for (String s : supported) {
+            FitnessAndQuality fitnessAndQuality = fitnessAndQualityParsed(s);
             fitnessAndQuality.mimeType = s;
             weightedMatches.add(fitnessAndQuality);
         }
@@ -271,12 +239,6 @@ public final class MIMEParse
 
         FitnessAndQuality lastOne = weightedMatches
                 .get(weightedMatches.size() - 1);
-        return NumberUtils.compare(lastOne.quality, 0) != 0 ? lastOne.mimeType
-                : "";
-    }
-
-    // hidden
-    private MIMEParse()
-    {
+        return NumberUtils.compare(lastOne.quality, 0) != 0 ? lastOne.mimeType : "";
     }
 }
