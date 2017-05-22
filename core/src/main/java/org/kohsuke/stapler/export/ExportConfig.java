jenkinsci/stapler

commit a9fd82eab8b7e6de90112dc7aea4b09a37ca98c9

JENKINS-40088# Configurable stapler serialization behavior (#106)
* JENKINS-40088# Configurable stapler serialization behavior

* Incorporated Jesse feedback on PR

* Reverted unnecessary edit

* Revert ExportedConfig changes to address compatibility concerns

* Incorporated Kohsuke's comments

- Fix javadoc
- ExportInterceptor should work off ExportConfig to use skipIfFail flag

* ExportInterceptor constat Skip.True to be used to skip writing property

* Use constnat object to represent SKIP

core/src/main/java/org/kohsuke/stapler/export/ExportConfig.java
@@ -14,6 +14,15 @@ public class ExportConfig {
 
     private ClassAttributeBehaviour classAttribute = ClassAttributeBehaviour.IF_NEEDED;
 
+    private ExportInterceptor exportInterceptor = ExportInterceptor.DEFAULT;
+
+    private boolean skipIfFail = false;
+
+    private Flavor flavor = Flavor.JSON;
+
+    /**
+     * If true, output will be indented to make it easier for humans to understand.
+     */
     public boolean isPrettyPrint() {
         return prettyPrint;
     }
@@ -36,6 +45,42 @@ public class ExportConfig {
     public ExportConfig withClassAttribute(ClassAttributeBehaviour cab) {
         if (cab==null)  throw new NullPointerException();
         this.classAttribute = cab;
+        return this;
+    }
+
+    /**
+     * Controls serialization of {@link @Exported} properties.
+     */
+    public ExportInterceptor getExportInterceptor() {
+        return exportInterceptor;
+    }
+
+    public ExportConfig withExportInterceptor(ExportInterceptor interceptor){
+        this.exportInterceptor = interceptor;
+        return this;
+    }
+
+    public ExportConfig withSkipIfFail(boolean skipIfFail){
+        this.skipIfFail = skipIfFail;
+        return this;
+    }
+
+    /**
+     * Turn on or off pretty printing of serialized data.
+     */
+    public boolean isSkipIfFail() {
+        return skipIfFail;
+    }
+
+    /**
+     * Gives {@link Flavor}
+     */
+    public Flavor getFlavor(){
+        return flavor;
+    }
+
+    public ExportConfig withFlavor(Flavor flavor){
+        this.flavor = flavor;
         return this;
     }
 }

core/src/main/java/org/kohsuke/stapler/ResponseImpl.java
@@ -23,37 +23,37 @@
 
 package org.kohsuke.stapler;
 
-import net.sf.json.JsonConfig;
-import org.kohsuke.stapler.compression.CompressionFilter;
-import org.kohsuke.stapler.compression.FilterServletOutputStream;
-import org.kohsuke.stapler.export.DataWriter;
-import org.kohsuke.stapler.export.ExportConfig;
-import org.kohsuke.stapler.export.NamedPathPruner;
-import org.kohsuke.stapler.export.Flavor;
-import org.kohsuke.stapler.export.Model;
-import org.kohsuke.stapler.export.ModelBuilder;
-import org.apache.commons.io.IOUtils;
-
-import javax.annotation.Nonnull;
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletResponseWrapper;
-import javax.servlet.http.HttpServletRequest;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.io.InputStream;
-import java.io.PrintWriter;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.io.Writer;
-import java.net.URL;
 import java.net.HttpURLConnection;
-import com.jcraft.jzlib.GZIPOutputStream;
+import java.net.URL;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import javax.annotation.Nonnull;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+import com.jcraft.jzlib.GZIPOutputStream;
+import net.sf.json.JsonConfig;
+import org.apache.commons.io.IOUtils;
+import org.kohsuke.stapler.compression.CompressionFilter;
+import org.kohsuke.stapler.compression.FilterServletOutputStream;
+import org.kohsuke.stapler.export.DataWriter;
+import org.kohsuke.stapler.export.ExportConfig;
+import org.kohsuke.stapler.export.Flavor;
+import org.kohsuke.stapler.export.Model;
+import org.kohsuke.stapler.export.ModelBuilder;
+import org.kohsuke.stapler.export.NamedPathPruner;
 import org.kohsuke.stapler.export.TreePruner;
 import org.kohsuke.stapler.export.TreePruner.ByDepth;
 
@@ -231,7 +231,13 @@ public class ResponseImpl extends HttpServletResponseWrapper implements StaplerR
 
     @SuppressWarnings({"unchecked", "rawtypes"}) // API design flaw prevents this from type-checking
     public void serveExposedBean(StaplerRequest req, Object exposedBean, Flavor flavor) throws ServletException, IOException {
+        serveExposedBean(req, exposedBean, new ExportConfig().withFlavor(flavor).withPrettyPrint(req.hasParameter("pretty")));
+    }
+
+    @Override
+    public void serveExposedBean(StaplerRequest req, Object exposedBean, ExportConfig config) throws ServletException, IOException {
         String pad=null;
+        Flavor flavor = config.getFlavor();
         setContentType(flavor.contentType);
         Writer w = getCompressedWriter(req);
 
@@ -239,7 +245,7 @@ public class ResponseImpl extends HttpServletResponseWrapper implements StaplerR
             pad = req.getParameter("jsonp");
             if(pad!=null) w.write(pad+'(');
         }
-        
+
         TreePruner pruner;
         String tree = req.getParameter("tree");
         if (tree != null) {
@@ -260,10 +266,6 @@ public class ResponseImpl extends HttpServletResponseWrapper implements StaplerR
             }
             pruner = new ByDepth(1 - depth);
         }
-
-        ExportConfig config = new ExportConfig();
-        config.prettyPrint = req.hasParameter("pretty");
-
         DataWriter dw = flavor.createDataWriter(exposedBean, w, config);
         if (exposedBean instanceof Object[]) {
             // TODO: extend the contract of DataWriter to capture this

core/src/main/java/org/kohsuke/stapler/StaplerResponse.java
@@ -24,20 +24,22 @@
 package org.kohsuke.stapler;
 
 import net.sf.json.JsonConfig;
+import org.kohsuke.stapler.export.DataWriter;
+import org.kohsuke.stapler.export.ExportConfig;
 import org.kohsuke.stapler.export.Flavor;
+import org.kohsuke.stapler.export.Model;
+import org.kohsuke.stapler.export.NamedPathPruner;
 
 import javax.annotation.Nonnull;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Writer;
 import java.net.URL;
 import java.net.URLConnection;
-import org.kohsuke.stapler.export.Model;
-import org.kohsuke.stapler.export.NamedPathPruner;
 
 /**
  * Defines additional operations made available by Stapler.
@@ -173,8 +175,28 @@ public interface StaplerResponse extends HttpServletResponse {
      * 
      * <p>As of 1.146, the {@code tree} parameter may be used to control the output
      * in detail; see {@link NamedPathPruner#NamedPathPruner(String)} for details.
+     *
+     * @deprecated Use {@link #serveExposedBean(StaplerRequest, Object, ExportConfig)}
      */
+    @Deprecated
     void serveExposedBean(StaplerRequest req, Object exposedBean, Flavor flavor) throws ServletException,IOException;
+
+    /**
+     * Serves the exposed bean in the specified flavor.
+     *
+     * <p>
+     * This method performs the complete output from the header to the response body.
+     * If the flavor is JSON, this method also supports JSONP via the {@code jsonp} query parameter.
+     *
+     * <p>The {@code depth} parameter may be used to specify a recursion depth
+     * as in {@link Model#writeTo(Object,int,DataWriter)}
+     *
+     * <p>As of 1.146, the {@code tree} parameter may be used to control the output
+     * in detail; see {@link NamedPathPruner#NamedPathPruner(String)} for details.
+     *
+     * <p> {@link ExportConfig} is passed by the caller to control serialization behavior
+     */
+    void serveExposedBean(StaplerRequest req, Object exposedBean, ExportConfig exportConfig) throws ServletException,IOException;
 
     /**
      * Works like {@link #getOutputStream()} but tries to send the response

core/src/main/java/org/kohsuke/stapler/StaplerResponseWrapper.java
@@ -1,5 +1,14 @@
 package org.kohsuke.stapler;
 
+import net.sf.json.JsonConfig;
+import org.kohsuke.stapler.export.ExportConfig;
+import org.kohsuke.stapler.export.Flavor;
+
+import javax.annotation.Nonnull;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -7,18 +16,6 @@ import java.io.PrintWriter;
 import java.io.Writer;
 import java.net.URL;
 import java.util.Locale;
-
-import javax.annotation.Nonnull;
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-
-import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.StaplerResponse;
-import org.kohsuke.stapler.export.Flavor;
-
-import net.sf.json.JsonConfig;
 
 /**
  * A basic wrapper for a StaplerResponse, e.g. in order to override some method.
@@ -139,9 +136,17 @@ public abstract class StaplerResponseWrapper implements StaplerResponse {
 
     /** {@inheritDoc} */
     @Override
+    @Deprecated
     public void serveExposedBean(StaplerRequest req, Object exposedBean, Flavor flavor)
             throws ServletException, IOException {
         getWrapped().serveExposedBean(req, exposedBean, flavor);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void serveExposedBean(StaplerRequest req, Object exposedBean, ExportConfig exportConfig)
+            throws ServletException, IOException {
+        getWrapped().serveExposedBean(req, exposedBean, exportConfig);
     }
 
     /** {@inheritDoc} */

core/src/main/java/org/kohsuke/stapler/export/DataWriter.java
@@ -23,6 +23,7 @@
 
 package org.kohsuke.stapler.export;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.IOException;
 import java.lang.reflect.Type;
@@ -68,6 +69,8 @@ public interface DataWriter {
     void type(@Nullable Type expected, @Nullable Class actual) throws IOException;
     void startObject() throws IOException;
     void endObject() throws IOException;
+
+    @Nonnull ExportConfig getExportConfig();
 
     /**
      * Recommended property name to write out the 'type' parameter of {@link #type(Type,Class)}

core/src/main/java/org/kohsuke/stapler/export/ExportInterceptor.java
@@ -1,0 +1,50 @@
+package org.kohsuke.stapler.export;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Allows caller to intercept exporting of properties.
+ *
+ * Implementation can choose to ignore properties in case of failure during serialization.
+ *
+ * @author Vivek Pandey
+ * @author James Dumay
+ */
+public abstract class ExportInterceptor {
+    /**
+     * Constant to tell if return of {@link ExportInterceptor#getValue(Property, Object, ExportConfig)} should be skipped.
+     *
+     * Constant to skip serializaing a property in case of error
+     */
+    public static final Object SKIP = new Object();
+
+    /**
+     * Subclasses must call {@link Property#getValue(Object)}  to retrieve the property.
+     *
+     * If the subclass decides the value can be included in the request return the value
+     * otherwise, return {@link #SKIP}  to skip the property.
+     *
+     * @param property to get the value from model object
+     * @param model object with this property
+     * @return the value of the property, if {@link #SKIP} is returned, this property will be skipped
+     * @throws IOException if there was a problem with serialization that should prevent
+     *         the serialization from proceeding
+     * @see Exported#skipNull()
+     */
+    public abstract Object getValue(Property property, Object model, ExportConfig config) throws IOException;
+
+    public static final ExportInterceptor DEFAULT = new ExportInterceptor() {
+        @Override
+        public Object getValue(Property property, Object model, ExportConfig config) throws IOException {
+            try {
+                return property.getValue(model);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                if(config.isSkipIfFail()) {
+                    return SKIP;
+                }
+                throw new IOException("Failed to write " + property.name + ":" + e.getMessage(), e);
+            }
+        }
+    };
+}

core/src/main/java/org/kohsuke/stapler/export/FieldProperty.java
@@ -25,7 +25,6 @@ package org.kohsuke.stapler.export;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
-import java.io.IOException;
 
 /**
  * {@link Property} based on {@link Field}.
@@ -51,7 +50,7 @@ class FieldProperty extends Property {
         return parent.getJavadoc().getProperty(field.getName());
     }
 
-    protected Object getValue(Object object) throws IllegalAccessException {
+    public Object getValue(Object object) throws IllegalAccessException {
         return field.get(object);
     }
 }

core/src/main/java/org/kohsuke/stapler/export/Flavor.java
@@ -23,10 +23,10 @@
 
 package org.kohsuke.stapler.export;
 
-import org.kohsuke.stapler.StaplerResponse;
-
 import java.io.IOException;
 import java.io.Writer;
+
+import org.kohsuke.stapler.StaplerResponse;
 
 /**
  * Export flavor.
@@ -73,7 +73,7 @@ public enum Flavor {
         return createDataWriter(bean,rsp.getWriter());
     }
     public DataWriter createDataWriter(Object bean, Writer w) throws IOException {
-        return createDataWriter(bean,w,new ExportConfig());
+        return createDataWriter(bean,w,new ExportConfig().withFlavor(this));
     }
     public abstract DataWriter createDataWriter(Object bean, Writer w, ExportConfig config) throws IOException;
 }

commit 13effff18f6595375b4baa9ed9e88e218277fa89

Javadoc error introduced in #106. Jenkinsfile is supposed to be defending against this. Yet docker run -v ~/.m2:/var/maven/.m2 -ti --rm --name stapler -u $(id -u):$(id -g) -e MAVEN_CONFIG=/var/maven/.m2 -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3.5.0-jdk-8 mvn -Duser.home=/var/maven javadoc:javadoc printed only 20 warnings for the core module, whereas locally (with the same Maven 3.5.0 + JDK 8u131) I get 100 warnings (i.e., truncated) plus this error! Adding -X did not track down any difference in configuration.

core/src/main/java/org/kohsuke/stapler/export/ExportConfig.java
@@ -49,7 +49,7 @@ public class ExportConfig {
     }
 
     /**
-     * Controls serialization of {@link @Exported} properties.
+     * Controls serialization of {@link Exported} properties.
      */
     public ExportInterceptor getExportInterceptor() {
         return exportInterceptor;