055fce8fd26aaa4098751d428af0d23672424b85
Daniel Beck <daniel-beck@users.noreply.github.com>
2017-12-10 02:30:44 +0100

    [JENKINS-34254] Allow customizing RequirePOST error pages

--- core/src/main/java/org/kohsuke/stapler/interceptor/RequirePOST.java
+++ core/src/main/java/org/kohsuke/stapler/interceptor/RequirePOST.java
@@ -2,6 +2,8 @@ package org.kohsuke.stapler.interceptor;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import org.kohsuke.stapler.ForwardToView;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
@@ -31,11 +33,35 @@ import org.kohsuke.stapler.verb.POST;
 @Target({METHOD,FIELD})
 @InterceptorAnnotation(RequirePOST.Processor.class)
 public @interface RequirePOST {
+
+    /**
+     * Allows customizing the error page shown when an annotated method is called with the wrong HTTP method.
+     */
+    interface ErrorHandler {
+        ForwardToView getForwardView();
+    }
+
     public static class Processor extends Interceptor {
+        private static ErrorHandler handler;
+
+        /**
+         * Register the custom error handler to be used when an annotated method is called with the wrong HTTP method.
+         * @param errorHandler the error handler providing the view to show instead
+         */
+        public static void registerErrorHandler(ErrorHandler errorHandler) {
+            handler = errorHandler;
+        }
+
         @Override
         public Object invoke(StaplerRequest request, StaplerResponse response, Object instance, Object[] arguments)
                 throws IllegalAccessException, InvocationTargetException, ServletException {
             if (!request.getMethod().equals("POST")) {
+                if (handler != null) {
+                    ForwardToView forwardToView = handler.getForwardView();
+                    if (forwardToView != null) {
+                        throw new InvocationTargetException(forwardToView.with("requestURL", request.getRequestURLWithQueryString().toString()));
+                    }
+                }
                 throw new InvocationTargetException(new HttpResponses.HttpResponseException() {
                     public void generateResponse(StaplerRequest req, StaplerResponse rsp, Object node) throws IOException, ServletException {
                         rsp.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
