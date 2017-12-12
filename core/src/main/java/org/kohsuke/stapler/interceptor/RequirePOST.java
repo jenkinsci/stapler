da0d0d6fc37ee1d8db01259fe7ac605c14c43d7f
Daniel Beck <daniel-beck@users.noreply.github.com>
2017-12-12 21:46:51 +0100

    [JENKINS-34254] Add nullability annotation and Javadoc

--- core/src/main/java/org/kohsuke/stapler/interceptor/RequirePOST.java
+++ core/src/main/java/org/kohsuke/stapler/interceptor/RequirePOST.java
@@ -14,6 +14,8 @@ import java.util.ServiceLoader;
 
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.*;
+
+import javax.annotation.CheckForNull;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import org.kohsuke.stapler.HttpResponses;
@@ -37,11 +39,15 @@ public @interface RequirePOST {
 
     /**
      * Allows customizing the error page shown when an annotated method is called with the wrong HTTP method.
-     *
-     * <p>Implementations are looked up using {@link java.util.ServiceLoader}, the first implementation to return a non-null value will be used.</p>
      */
     interface ErrorCustomizer {
-        ForwardToView getForwardView();
+        /**
+         * Return the {@link ForwardToView} showing a custom error page for {@link RequirePOST} annotated methods. This is
+         * typically used to show a form with a "Try again using POST" button.
+         *
+         * <p>Implementations are looked up using {@link java.util.ServiceLoader}, the first implementation to return a non-null value will be used.</p>
+         */
+        @CheckForNull ForwardToView getForwardView();
     }
 
     public static class Processor extends Interceptor {
