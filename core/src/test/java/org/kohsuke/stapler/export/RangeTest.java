6850324e779b72052bf21bdfd9673addc8659d2f
Basil Crow <me@basilcrow.com>
2021-01-30 20:31:56 -0800

    Remove usages of com.google.common.collect.Iterables.

--- core/src/test/java/org/kohsuke/stapler/export/RangeTest.java
+++ core/src/test/java/org/kohsuke/stapler/export/RangeTest.java
@@ -1,7 +1,8 @@
 package org.kohsuke.stapler.export;
 
-import com.google.common.collect.Iterables;
-import org.junit.Assert;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,7 +11,7 @@ import java.util.List;
 import java.util.Set;
 
 
-public class RangeTest extends Assert {
+public class RangeTest {
     String[] array = new String[]{"a", "b", "c", "d", "e", "f"};
     List<String> list = Arrays.asList(array);
     Set<String> set = new LinkedHashSet<String>(list);
@@ -18,28 +19,24 @@ public class RangeTest extends Assert {
     @Test
     public void normalRange() {
         Range r = new Range(2,4);
-        assertEquals("[c, d]", toS(r.apply(array)));
-        assertEquals("[c, d]", toS(r.apply(list)));
-        assertEquals("[c, d]", toS(r.apply(set)));
+        assertThat(r.apply(array), contains("c", "d"));
+        assertThat(r.apply(list), contains("c", "d"));
+        assertThat(r.apply(set), contains("c", "d"));
     }
 
     @Test
     public void maxOnlyRange() {
         Range r = new Range(-1,2);
-        assertEquals("[a, b]", toS(r.apply(array)));
-        assertEquals("[a, b]", toS(r.apply(list)));
-        assertEquals("[a, b]", toS(r.apply(set)));
+        assertThat(r.apply(array), contains("a", "b"));
+        assertThat(r.apply(list), contains("a", "b"));
+        assertThat(r.apply(set), contains("a", "b"));
     }
 
     @Test
     public void minOnlyRange() {
         Range r = new Range(4,Integer.MAX_VALUE);
-        assertEquals("[e, f]", toS(r.apply(array)));
-        assertEquals("[e, f]", toS(r.apply(list)));
-        assertEquals("[e, f]", toS(r.apply(set)));
-    }
-
-    private String toS(Iterable i) {
-        return Iterables.toString(i);
+        assertThat(r.apply(array), contains("e", "f"));
+        assertThat(r.apply(list), contains("e", "f"));
+        assertThat(r.apply(set), contains("e", "f"));
     }
 }
