2aeffece2bad8a42808aea96aff791ad6a79fab9
Basil Crow <me@basilcrow.com>
2022-08-08 09:37:40 -0700

    Migrate from Hickory to Elementary (#394)

--- core/src/test/java/org/kohsuke/stapler/jsr269/QueryParameterAnnotationProcessorTest.java
+++ core/src/test/java/org/kohsuke/stapler/jsr269/QueryParameterAnnotationProcessorTest.java
@@ -1,27 +1,36 @@
 package org.kohsuke.stapler.jsr269;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.karuslabs.elementary.Results;
+import com.karuslabs.elementary.junit.JavacExtension;
+import com.karuslabs.elementary.junit.annotations.Inline;
+import com.karuslabs.elementary.junit.annotations.Options;
+import com.karuslabs.elementary.junit.annotations.Processors;
 import java.util.Collections;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import net.java.dev.hickory.testing.Compilation;
-import org.junit.Test;
+@ExtendWith(JavacExtension.class)
+@Options("-Werror")
+@Processors(QueryParameterAnnotationProcessor.class)
+class QueryParameterAnnotationProcessorTest {
 
-import static org.junit.Assert.*;
-
-public class QueryParameterAnnotationProcessorTest {
-
-    @Test public void basicOutput() {
-        Compilation compilation = new Compilation();
-        compilation.addSource("some.pkg.Stuff").
-                addLine("package some.pkg;").
-                addLine("import org.kohsuke.stapler.QueryParameter;").
-                addLine("public class Stuff {").
-                addLine("  public void doOneThing(@QueryParameter String key) {}").
-                addLine("  public void doAnother(@QueryParameter(\"ignoredHere\") String name, @QueryParameter String address) {}").
-                addLine("}");
-        compilation.doCompile(null, "-source", "8", "-Xlint:none");
-        assertEquals(Collections.emptyList(), compilation.getDiagnostics());
-        assertEquals("key", Utils.getGeneratedResource(compilation, "some/pkg/Stuff/doOneThing.stapler"));
-        assertEquals("name,address", Utils.getGeneratedResource(compilation, "some/pkg/Stuff/doAnother.stapler"));
+    @Inline(
+            name = "some.pkg.Stuff",
+            source = {
+                "package some.pkg;",
+                "import org.kohsuke.stapler.QueryParameter;",
+                "public class Stuff {",
+                "  public void doOneThing(@QueryParameter String key) {}",
+                "  public void doAnother(@QueryParameter(\"ignoredHere\") String name, @QueryParameter String address) {}",
+                "}",
+            })
+    @Test
+    void basicOutput(Results results) {
+        assertEquals(Collections.emptyList(), results.diagnostics);
+        assertEquals("key", Utils.getGeneratedResource(results.sources, "some/pkg/Stuff/doOneThing.stapler"));
+        assertEquals("name,address", Utils.getGeneratedResource(results.sources, "some/pkg/Stuff/doAnother.stapler"));
     }
 
     // TODO nested classes use qualified rather than binary name
