d867a2ef17e2f1cbaa62caf1c63ed891b93efa60
Basil Crow <me@basilcrow.com>
2025-07-31 09:06:51 -0700

    import

--- core/src/main/java/org/kohsuke/stapler/AcceptHeader.java
+++ core/src/main/java/org/kohsuke/stapler/AcceptHeader.java
@@ -29,6 +29,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.apache.commons.beanutils.Converter;
 
 /**
